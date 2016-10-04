b1453b2924a6104649adb71d724b9ef4236835d5
odins41 <odins41@mail.ru>
2016-10-04 14:41:51 +0300

    Add files via upload

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,18 @@
-
+from collections import Counter
+import re
 
 def load_data(filepath):
-    pass
+    text_file = open(file_path)
+    text_string = text_file.read()
+    return text_string
+    
 
-
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(file_path):
+    words = re.findall('\w+', open(file_path).read().lower())
+    print (Counter(words).most_common(10))
+    
 
 
 if __name__ == '__main__':
-    pass
+    file_path = input()
+    get_most_frequent_words(file_path)
