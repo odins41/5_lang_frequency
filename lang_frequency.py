2265645a1aec2004ac4477e0f178044e503935c0
odins41 <odins41@mail.ru>
2016-10-31 21:46:20 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,14 +4,18 @@ import re
 def load_data(filepath):
     with open(filepath) as text_file:
         text = text_file.read()
+        text = text.lower()
         return text
     
 
 def get_most_frequent_words(text_file):
+    most_frequent_words = []
     number_common_words = 10
-    words = re.findall('\w+', text_file)
-    frequency = Counter(words).most_common(number_common_words)
-    return frequency
+    all_words = re.findall('\w+', text_file)
+    frequency = Counter(all_words).most_common(number_common_words)
+    for words in frequency:
+         most_frequent_words.append(words[0])    
+    return most_frequent_words
 
 
 if __name__ == '__main__':
